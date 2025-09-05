957fb2cf7ac455e3c301cba852c45ccccb28d061
ZwelishaNgcongo <zwelisha.ngcongo@gmail.com>
2025-09-05 16:19:50 +0200

    updated email and contacts to avoid using unique constraints

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -269,7 +269,7 @@ const FormModal = ({
                 className="absolute top-4 right-4 cursor-pointer"
                 onClick={() => setOpen(false)}
               >
-                <Image src="/close.png" alt="" width={14} height={14} />
+                
               </div>
             )}
           </div>
